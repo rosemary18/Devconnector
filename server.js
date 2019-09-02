cbcb9781e3487dcf45ac5c6c59caa4bd4982a210
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2019-09-02 10:25:09 +0700

    Repair server

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-if (process.env.NODE_ENV) {
-  app.use(express / static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
