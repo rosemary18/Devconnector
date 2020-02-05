80ca9b707ef9568685f3bb88c49fbe52d138ebde
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2020-02-05 13:29:54 +0700

    change footer #2

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} DevConnector ( Created by{" "}
-      <a href="https://github.com/Rosemary18">Rosemary18</a>)
+      Copyright &copy; {new Date().getFullYear()} DevConnector. Created by{" "}
+      <a href="https://github.com/Rosemary18">Rosemary18</a>
     </footer>
   );
 };
